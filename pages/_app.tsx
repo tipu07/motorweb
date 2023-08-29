533d1d1deb146eec71abb0a3e1698d4668381fba
Tipu <30400782+tipu07@users.noreply.github.com>
2023-08-29 13:34:57 +0500

    Change Proj Structure

--- pages/_app.tsx
+++
@@ -1,8 +0,0 @@
-import "@/styles/css/bootstrap.min.css"
-import "@/styles/css/slick.min.css"
-import "@/styles/scss/web.generic.scss"
-import type { AppProps } from "next/app"
-
-export default function App({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />
-}
