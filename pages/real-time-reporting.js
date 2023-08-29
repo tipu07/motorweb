2501221ced29d52fe937c50942f72e5d6da9b8ea
Tipu <30400782+tipu07@users.noreply.github.com>
2023-08-29 14:54:53 +0500

    Fix Vercel Bug

--- pages/real-time-reporting.js
+++ pages/real-time-reporting.js
@@ -11,7 +11,7 @@ export default function RealTimeReporting() {
 	return (
 		<>
 			<Head>
-				<title>Real Time Reporting / API's — Motor Web</title>
+				<title>Real Time Reporting / API&lsquo;s — Motor Web</title>
 				<meta name="description" content="Motor Web" />
 				<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=2, user-scalable=yes" />
 				<link rel="icon" href="/images/favicon.png" />
